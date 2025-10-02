71d9433902e54ef83749f9cfff87322ec7d77b7b
Alisson <alissondedeussiqueira@gmail.com>
2025-10-02 14:08:13 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9758448529-f9b22",
+  "appId": "1:947978358911:web:37e5439585b169d1732f4a",
+  "apiKey": "AIzaSyAB92gHNwmPnoRor05O3eO1zeR45r6k1yg",
+  "authDomain": "studio-9758448529-f9b22.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "947978358911"
+};
