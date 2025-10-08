06c24bd47377db58c0dd0a2486ca26df5c0223aa
Alisson <alissondedeussiqueira@gmail.com>
2025-10-08 19:08:56 +0000

    Try fixing this error: `Runtime Error: Error: React is not defined. Erro

--- src/app/dashboard/module/[moduleId]/page.tsx
+++ src/app/dashboard/module/[moduleId]/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'next/navigation';
 import { useUser } from '@/firebase';
 import { modules } from '@/lib/modules';
